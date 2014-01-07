1851f5058b61e0e2dfebfc12ac92f08bf9b36265
Umais Siddiqui <umais20@yahoo.com>
2014-01-07 13:03:39 -0500

    Added a Site JS File

---
+++ HeartbeatBusiness/Scripts/script.js
@@ -0,0 +1,18 @@
+﻿
+var things = function () {
+
+    return 'The thing function';
+    
+}
+
+console.log(things());
+
+
+var ninja = {
+
+    yell: function yell(n) {
+        return n > 0 ? yell(n - 1) + 'a' : 'hiy';
+    }
+}
+
+assert(ninja.yell(4) == 'hiyaaaa', 'Works as we expect it to!');

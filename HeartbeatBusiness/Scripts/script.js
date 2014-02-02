d198261de4f5e632d461ef043ea503c22bf40f53
Umais Siddiqui <umais20@yahoo.com>
2014-02-02 06:21:00 -0500

    lots of changes

--- HeartbeatBusiness/Scripts/script.js
+++ HeartbeatBusiness/Scripts/script.js
@@ -1,30 +1,73 @@
 ﻿
-function assert(value,desc) {
+$(document).ready(function () {
+    jQuery.support.cors = true;
+   // $('#startDate').change(function () { $('#UKPrice').focus(); });
+    $('#startDate').datepicker();
+    $('#endDate').datepicker();
 
-    var li = document.createElement("li");
-    li.className = value ? "pass" : "fail";
-    li.appendChild(document.createTextNode(desc));
-    document.getElementById("results").appendChild(li);
+});
 
+
+function NullifyObject(obj) {
+    if (obj.ISBN13 === "")
+        obj.ISBN13 = null;
+    if (obj.title === "")
+        obj.title = null;
+    if (obj.author === "")
+        obj.author = null;
+
+    return obj;
+}
+
+function showMessage(isVisible) {
+    if (isVisible) {
+        $('#dialog-confirm').dialog('open');
+    }
+    else {
+        $('#dialog-confirm').dialog('close');
+    }
 }
 
 
-var things = function () {
 
-    return 'The thing function';
+
+
+
+
+
+
+
+
+
+
+
+
+//function assert(value,desc) {
+
+//    var li = document.createElement("li");
+//    li.className = value ? "pass" : "fail";
+//    li.appendChild(document.createTextNode(desc));
+//    document.getElementById("results").appendChild(li);
+
+//}
+
+
+//var things = function () {
+
+//    return 'The thing function';
     
-}
+//}
 
 //console.log(things());
 
 
-var ninja = {
+//var ninja = {
 
-    yell: function yell(n) {
-        return n > 0 ? yell(n - 1) + 'a' : 'hiy';
-    }
-}
+//    yell: function yell(n) {
+//        return n > 0 ? yell(n - 1) + 'a' : 'hiy';
+//    }
+//}
 
-assert(ninja.yell(4) == 'hiyaaaa', 'Works as we expect it to!');
+//assert(ninja.yell(4) == 'hiyaaaa', 'Works as we expect it to!');
 
-assert(ninja.yell(4) == 'hiyaaa', 'Ahh Man this failed!');
+//assert(ninja.yell(4) == 'hiyaaa', 'Ahh Man this failed!');
